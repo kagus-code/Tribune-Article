77421d4793708abb56aac304c1ce22b0d51b0555
Eston Kagwima <ekagwima745@gmail.com>
2021-05-11 11:43:04 +0300

    adding css,bootstrap,dynamicLinks,

--- news/urls.py
+++ news/urls.py
@@ -1,11 +1,11 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from . import views
 
 urlpatterns = [
     path('home/', views.welcome, name='welcome'),
-    path('today/',views.news_of_day, name='newsToday'),
-    path('archives/(\d{4}-\d{2}-\d{2})/',views.past_days_news,name = 'pastNews') ] 
+    path('today/', views.news_of_day, name='newsToday'),
+    re_path(r'^archives/(\d{4}-\d{2}-\d{2})/$', views.past_days_news, name='pastNews')]
 
 
-]
+
