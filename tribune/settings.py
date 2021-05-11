88b2220ae01dfa6c7699d187c8020e492d3ea808
Eston Kagwima <ekagwima745@gmail.com>
2021-05-11 14:08:16 +0300

    create relationships in the DB

--- tribune/settings.py
+++ tribune/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = 'tribune.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'tribune',
+        'USER': 'kagus',
+        'PASSWORD': 'Muraya11$',
     }
 }
 
@@ -107,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
