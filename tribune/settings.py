0be2c142393febd2dd80778eafb66605d0cdfc5c
Eston Kagwima <ekagwima745@gmail.com>
2021-05-13 00:40:36 +0300

    install gunicorns

--- tribune/settings.py
+++ tribune/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 MODE=config("MODE", default="dev")
 SECRET_KEY = config('SECRET_KEY')
-DEBUG = os.environ.get('DEBUG', False)
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 # development
 if config('MODE')=="dev":
