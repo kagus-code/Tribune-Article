5533491a7dc328748b9c1b6e0b4d78e4b59e4e49
Eston Kagwima <ekagwima745@gmail.com>
2021-05-13 00:33:07 +0300

    heroku push

--- tribune/settings.py
+++ tribune/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 MODE=config("MODE", default="dev")
 SECRET_KEY = config('SECRET_KEY')
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = os.environ.get('DEBUG', False)
 
 # development
 if config('MODE')=="dev":
