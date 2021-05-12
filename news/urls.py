e8e49b8374fbd6a48ce611ff2d61881939ed26ed
Eston Kagwima <ekagwima745@gmail.com>
2021-05-12 12:53:11 +0300

    adding search feature

--- news/urls.py
+++ news/urls.py
@@ -4,4 +4,12 @@ from . import views
 
 urlpatterns = [
     re_path(r'^$', views.news_of_day, name='newsToday'),
-    re_path(r'^archives/(\d{4}-\d{2}-\d{2})/$', views.past_days_news, name='pastNews')]
+    re_path(r'^archives/(\d{4}-\d{2}-\d{2})/$', views.past_days_news, name='pastNews'),
+    re_path(r'^search/', views.search_results, name='search_results')
+    
+    
+    
+    
+    ]
+    
+
