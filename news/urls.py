d7748553323489a15b2bd5229046a29fd62d49df
Eston Kagwima <ekagwima745@gmail.com>
2021-05-19 10:20:50 +0300

    editing single article url

--- news/urls.py
+++ news/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     re_path(r'^$', views.news_of_day, name='newsToday'),
     re_path(r'^archives/(\d{4}-\d{2}-\d{2})/$', views.past_days_news, name='pastNews'),
     re_path(r'^search/', views.search_results, name='search_results'),
-    re_path(r'^article/(\d+)',views.article,name ='article'),
+    re_path(r'^article/(?P<article_id>\d+)',views.article,name ='article'),
     re_path(r'^new/article$', views.new_article, name='new-article'),
 
 
