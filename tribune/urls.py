f02a4c33cd70946912eccbc2c51177696b8a92fe
Eston Kagwima <ekagwima745@gmail.com>
2021-05-19 06:21:12 +0300

    removing unwanted comments in admin urls

--- tribune/urls.py
+++ tribune/urls.py
@@ -21,14 +21,11 @@ urlpatterns = [
     re_path(r'^admin/', admin.site.urls),
     re_path(r'', include('news.urls')),
 
-    # path('accounts/', include('django_registration.backends.one_step.urls')),
-    # path('accounts/', include('django.contrib.auth.urls')),
-    # 
 
-    # path('accounts/register/', RegistrationView.as_view(success_url='/'),name='django_registration_register'),
-    # re_path(r'^accounts/', include('django_registration.backends.one_step.urls')),
-    # re_path(r'^accounts/', include('django.contrib.auth.urls')),
-    # 
-    # 
+
+
+
+
+
 
 ]
